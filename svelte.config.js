a581bad5c86bd89b5b155092210b863e7327bc32
marcograhl <{ID}+{username}@users.noreply.github.com>
2023-11-10 14:44:05 +0100

    added node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 import postcssPresetEnv from 'postcss-preset-env';
 import atImport from 'postcss-import';
